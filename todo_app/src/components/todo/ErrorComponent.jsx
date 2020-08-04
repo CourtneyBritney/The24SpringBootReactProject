02ad81e33dfd60c8a7f87eab52cd3e8ebb2eff17
CourtneyBritney <courtney02alexander@gmail.com>
2020-08-04 13:27:36 +0200

    Added Files

---
+++ todo_app/src/components/todo/ErrorComponent.jsx
@@ -0,0 +1,13 @@
+//*** ErrorComponent.jsx ***\\
+
+import React from 'react';
+
+function ErrorComponent() {
+    return (
+        <div>
+            An error occured. I don't know what to do! Contact support at abcd-efgh-ijkl
+        </div>
+    )
+}
+
+export default ErrorComponent;
---
+++ todo_app/src/components/todo/FooterComponent.jsx
@@ -0,0 +1,17 @@
+//*** FooterComponent.jsx ***\\
+
+import React, { Component } from 'react';
+// import AuthenticationService from'./AuthenticationService.js';
+// import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+class FooterComponent extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                <span className="text-muted"> All Rights Reserved 2020 @Courtney </span>                
+            </footer>
+        )
+    }
+}
+
+export default FooterComponent;
